f179ccdc66501405fab2fe109cc666f4431100ec
Rafael Feodrippe <pfeodrippe@gmail.com>
2020-05-26 00:36:12 -0300

    seq

--- src/tla_web_repl/core.clj
+++ src/tla_web_repl/core.clj
@@ -58,7 +58,7 @@ Spec == Ini
 
   (eval-tla "" "3 + 5")
 
-  (eval-tla "Pitoco(B) == ~B" "Pitoco(TRUE)")
+  (eval-tla "" "<<4, 5, 5>> \\o <<10>>")
 
   (eval-tla "IndexOf(seq, elem) == CHOOSE i \\in 1..Len(seq): seq[i] = elem"
             "IndexOf(<<3, 6, 7>>, 3)")
