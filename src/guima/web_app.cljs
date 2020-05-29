0dfb56a55fc182ccf886a2379acdf10d45dcbb38
Rafael Feodrippe <pfeodrippe@gmail.com>
2020-05-29 00:23:29 -0300

    ss

--- src/guima/web_app.cljs
+++ src/guima/web_app.cljs
@@ -1,5 +1,24 @@
-(ns guima.web-app)
+(ns guima.web-app
+  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require [cljs-http.client :as http]
+            [cljs.core.async :refer [<!]]))
+
+(defn- json-parse
+  [s]
+  (js->clj (js/JSON.parse s)))
+
+(defn eval-handler
+  [cm]
+  (go (let [response (<! (http/post (str (.. js/window -location -origin) "/api/eval-tla-expression")
+                                    {:json-params {:input (.getValue cm)}}))
+            result-el (.. cm getWrapperElement -parentNode (querySelector "#result"))]
+        (if (= (:status response) 200)
+          (do (set! (.-color (.-style result-el)) "#333333")
+              (set! (.-innerHTML result-el)
+                    (get (json-parse (:body response)) "data")))
+          (do (set! (.-color (.-style result-el)) "#cc0000")
+              (set! (.-innerHTML result-el)
+                    (get-in (json-parse (:body response)) ["error" "message"]) ))))))
 
 (defn main!
-  []
-  (+ 2 5))
+  [])
