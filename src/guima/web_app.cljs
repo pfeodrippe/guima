26f0cf1e558061e415f80d1bf25a21849f6f6e50
Rafael Feodrippe <pfeodrippe@gmail.com>
2020-05-31 14:17:17 -0300

    WIP 2

--- src/guima/web_app.cljs
+++ src/guima/web_app.cljs
@@ -8,7 +8,8 @@
    [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
    [com.fulcrologic.fulcro.dom :as d]
    ["codemirror/lib/codemirror" :as CodeMirror]
-   ["codemirror/mode/ruby/ruby"]))
+   ["codemirror/mode/ruby/ruby"]
+   ["codemirror/addon/edit/closebrackets"]))
 
 #_(print )
 #_(cm)
@@ -36,23 +37,52 @@
 
 (defonce app (app/fulcro-app))
 
-(defn code-mirror
+(defn add-code-mirror
   [parent]
-  (CodeMirror. (.getElementById js/document "app")
-               (clj->js {:mode "ruby"
-                         :viewportMargin js/Infinity})))
+  (when (and parent (.. parent -parentNode))
+    (let [cm (CodeMirror. (fn [el]
+                            (.. parent -parentNode (replaceChild el parent)))
+                          (clj->js {:mode "ruby"
+                                    :viewportMargin js/Infinity}))]
+      (doto cm
+        (.setSize "50%" "auto")
+        (.focus)
+        (.setOption "autoCloseBrackets" true)))))
 
 (defsc Repl [this {:keys [:cm]}]
-  {:initial-state (fn [_] {:cm (code-mirror 0)})}
-  (d/div (CodeMirror. (.getElementById js/document "app")
-                      (clj->js {:mode "ruby"
-                                :viewportMargin js/Infinity}))))
+  {:initial-state (fn [_] {:cm 0})}
+  (d/div {:id "code-and-result",
+          :style {:display "flex", :marginTop "20px", :fontSize "20px"}}
+    (d/div {:ref add-code-mirror})
+    (d/div)))
 
 (def ui-repl (comp/factory Repl))
 
 (defsc Root [this {:keys [:repl]}]
   {:initial-state (fn [_] {:repl (comp/get-initial-state Repl {})})}
-  (d/div (ui-repl repl)))
+  (d/div {}
+    (d/div {:style {:display "flex"}}
+      (d/div {:style {:display "flex",
+                      :justifyContent "flex-start",
+                      :width "100%",
+                      :padding "10px 15px 7px 10px",
+                      :fontSize "14px",
+                      :color "#666666"}}
+        (d/b "Guima")
+        "| A TLA+ REPL")
+      (d/div {:style {:display "flex",
+                      :justifyContent "flex-end",
+                      :width "100%",
+                      :padding "0"}}
+        (d/a :.bmc-button
+          {:target "_blank",
+           :href "https://www.buymeacoffee.com/pfeodrippe",
+           :style {:transform "scale(0.5)"}}
+          (d/img {:src "https://cdn.buymeacoffee.com/buttons/bmc-new-btn-logo.svg"
+                  :alt "Buy me a coffee"})
+          (d/span {:style {:marginLeft "5px", :fontSize "19px !important"}}
+            "Buy me a coffee"))))
+    (ui-repl repl)))
 
 (defn ^:export refresh
   "During development, shadow-cljs will call this on every hot reload of source. See shadow-cljs.edn"
