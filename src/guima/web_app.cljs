1de93aca7f749e3d61a7b9e22d9fc96933a33160
Rafael Feodrippe <pfeodrippe@gmail.com>
2020-06-01 10:15:10 -0300

    remo

--- src/guima/web_app.cljs
+++ src/guima/web_app.cljs
@@ -35,37 +35,53 @@
 
 (defn add-code-mirror
   [parent]
-  (when (and parent (.querySelector parent "#editor"))
-    (let [editor-div (.querySelector parent "#editor")
-          result-div (.querySelector parent "#result")
-          cm (CodeMirror. (fn [el]
-                            (.. parent (replaceChild el editor-div)))
-                          (clj->js {:mode "ruby"
-                                    :viewportMargin js/Infinity
-                                    #_ #_:extraKeys {"Shift-Enter" eval-handler
-                                                "Ctrl-Enter" eval-handler}}))]
-      (doto cm
-        (.setSize "50%" "auto")
-        (.focus)
-        (.setOption "autoCloseBrackets" true)))))
+  (let [editor-div (.querySelector parent "#editor")
+        result-div (.querySelector parent "#result")
+        cm (CodeMirror. (fn [el]
+                          (.. parent (replaceChild el editor-div)))
+                        (clj->js {:mode "ruby"
+                                  :viewportMargin js/Infinity
+                                  #_ #_:extraKeys {"Shift-Enter" eval-handler
+                                                   "Ctrl-Enter" eval-handler}}))]
+    (doto cm
+      (.setSize "50%" "auto")
+      #_(.focus)
+      (.setOption "autoCloseBrackets" true))))
 
-(defsc Repl [this {:keys [:repl/id]} {:keys [:on-create]}]
-  {:query [:repl/id]
+;; Creates a new Repl component.
+;; `0` is the initial id.
+(defsc Repl [this {:keys [:repl/id :repl/editor :repl/focus]} {:keys [:on-create :on-delete]}]
+  {:query [:repl/id :repl/editor :repl/focus]
    :ident (fn [] [:repl/id id])
-   :initial-state (fn [{:keys [:repl/id]}] {:repl/id id})}
+   :initial-state (fn [{:keys [:repl/id]}]
+                    {:repl/id id :repl/editor nil :repl/focus false})}
+  (when focus
+    (.focus editor))
   (d/div {:id "code-and-result",
           :onKeyDown (fn [evt]
-                       (log evt)
                        (cond
                          (and (.-ctrlKey evt) (= (.-keyCode evt) 13))
-                         (do (log "OLA") (.preventDefault evt))
+                         (.preventDefault evt)
 
                          (and (.-altKey evt) (= (.-keyCode evt) 13))
                          (on-create id)
 
                          :else evt))
           :style {:display "flex", :marginTop "20px", :fontSize "20px"}
-          :ref add-code-mirror}
+          :ref (fn [ref]
+                 (when (and ref (.querySelector ref "#editor"))
+                   (let [cm (add-code-mirror ref)]
+                     (.on cm "beforeChange"
+                          (fn [cm evt]
+                            (when (and (not= id 0)
+                                       (empty? (.getValue cm))
+                                       (zero? (.. evt -to -line))
+                                       (zero? (.. evt -to -ch))
+                                       (= (.. evt -origin) "+delete"))
+                              (comp/transact! this [(api/delete-repl {:repl/id id})]))))
+                     (comp/transact! this [(api/add-repl-editor
+                                            {:repl/id id
+                                             :repl/editor cm})]))))}
     (d/div :#editor)
     (d/div {:id "result",
             :style {:marginLeft "10px",
@@ -76,9 +92,10 @@
 
 (def ui-repl (comp/computed-factory Repl {:keyfn :repl/id }))
 
-(defsc Root [this {:keys [:list/repls]}]
-  {:query [{:list/repls (comp/get-query Repl)}]
-   :initial-state (fn [_] {:list/repls [(comp/get-initial-state Repl {:repl/id 0})]})}
+(defsc Root [this {:keys [:list/repls :root/unique-id]}]
+  {:query [{:list/repls (comp/get-query Repl)} :root/unique-id]
+   :initial-state (fn [_] {:list/repls [(comp/get-initial-state Repl {:repl/id 0})]
+                           :root/unique-id 1})}
   (d/div {}
     (d/div {:style {:display "flex"}}
       (d/div {:style {:display "flex",
@@ -102,9 +119,10 @@
           (d/span {:style {:marginLeft "5px", :fontSize "19px !important"}}
             "Buy me a coffee"))))
     (let [on-create (fn [before-id]
-                      (comp/transact! this [(api/add-repl {:repl/id (count repls)
-                                                           :before-id before-id})]))]
-      (map #(ui-repl % {:on-create on-create}) repls))))
+                      (comp/transact! this [(api/add-repl {:repl/id unique-id
+                                                           :before-id before-id})]))
+          on-delete (fn [id] (comp/transact! this [(api/delete-repl {:repl/id id })]))]
+      (map #(ui-repl % {:on-create on-create :on-delete on-delete}) repls))))
 
 (defn ^:export refresh
   "During development, shadow-cljs will call this on every hot reload of source. See shadow-cljs.edn"
--- src/guima/web_app/mutation.cljs
+++ src/guima/web_app/mutation.cljs
@@ -3,11 +3,19 @@
    [com.fulcrologic.fulcro.mutations :as m :refer [defmutation]]
    [com.fulcrologic.fulcro.algorithms.merge :as merge]))
 
+(defn- remove-all-focus
+  [s]
+  (update s :repl/id (fn [id->repl]
+                       (->> (map (fn [[id repl]]
+                                   [id (assoc repl :repl/focus false)])
+                                 id->repl)
+                            (into {})))))
+
 (defmutation add-repl
   [{:keys [:repl/id :before-id]}]
   (action [{:keys [:state]}]
     (swap! state (fn [s]
-                   ;; find index position using `before-id`
+                   ;; find index position using `before-id` and put new repl after it
                    (let [new-list (reduce (fn [acc [ident old-id]]
                                             (if (= old-id before-id)
                                               (conj acc [ident old-id] [:repl/id id])
@@ -15,4 +23,25 @@
                                           [] (:list/repls s))]
                      (-> s
                          (assoc-in [:repl/id id] {:repl/id id})
-                         (assoc :list/repls new-list)))))))
+                         (assoc :list/repls new-list :root/unique-id (inc id))))))))
+
+(defmutation add-repl-editor
+  [{:keys [:repl/id :repl/editor]}]
+  (action [{:keys [:state]}]
+    (swap! state #(-> %
+                      remove-all-focus
+                      (update-in [:repl/id id] assoc :repl/editor editor :repl/focus true)))))
+
+(defmutation delete-repl
+  [{:keys [:repl/id]}]
+  (action [{:keys [:state]}]
+    (swap! state #(let [anterior-id (reduce (fn [anterior-id [ident old-id]]
+                                              (if (= old-id id)
+                                                (reduced anterior-id)
+                                                old-id))
+                                            nil (:list/repls %))]
+                    (-> %
+                        remove-all-focus
+                        (update :repl/id dissoc id)
+                        (update-in [:repl/id anterior-id] assoc :repl/focus true)
+                        (merge/remove-ident* [:repl/id id] [:list/repls]))))))
